d42358b4e1cc934b03718e1117d885f9390344b7
Michael Tarasov <Michael.Tarasov@tutanota.com>
2021-06-22 13:17:25 -0400

    Made image bar functional
    Signed-off-by: Michael Tarasov <Michael.Tarasov@tutanota.com>

--- src/Scripts/index.js
+++ src/Scripts/index.js
@@ -1,1 +1,23 @@
- 
+function SelectImage(image, itemlist) {
+	for (let i = 0; i < itemlist.length; i++) {
+		// Removes the selected image tag from all images
+		const item = itemlist.item(i);
+		item.classList.remove("selectedimage");
+	}
+	image.classList.add("selectedimage");
+
+    document.getElementsByClassName("ActiveImage")[0].innerHTML = `<img class="activeimage" src="${image.src}" />`
+}
+
+function MakeImagesClickable() {
+	const ActiveProjectImages = document.getElementsByClassName("sampleimage");
+
+	for (let i = 0; i < ActiveProjectImages.length; i++) {
+		const item = ActiveProjectImages.item(i);
+		item.addEventListener("click", function () {
+			SelectImage(item, ActiveProjectImages);
+		});
+	}
+}
+
+MakeImagesClickable();
