2b22504553fa5b04689fc21d02894e06852a9aca
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2021-07-25 21:03:03 -0400

    Switched Github logo from png to svg
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- src/Scripts/Logos.js
+++ src/Scripts/Logos.js
@@ -1,6 +1,6 @@
 const logos = {
 	VSCode: "https://raw.githubusercontent.com/MichaelTarasov21/Portfolio/master/src/Assets/VSCode.png",
-	Github: "https://raw.githubusercontent.com/MichaelTarasov21/Portfolio/master/src/Assets/Github.png",
+	Github: "https://raw.githubusercontent.com/MichaelTarasov21/Portfolio/master/src/Assets/Github-Light.svg",
 	WebFrontendStack: "https://raw.githubusercontent.com/MichaelTarasov21/Portfolio/master/src/Assets/Web-Stack.png",
 	Netlify: "https://raw.githubusercontent.com/MichaelTarasov21/Portfolio/master/src/Assets/Netlify.svg",
 };
