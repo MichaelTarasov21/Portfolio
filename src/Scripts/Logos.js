8921ae671a85539c3cc8b5c9116140e010baec3e
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2021-08-17 19:23:33 -0400

    Created a separate folder for development logos
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- src/Scripts/Logos.js
+++ src/Scripts/Logos.js
@@ -1,12 +1,12 @@
 const logos = {
-	VSCode: "https://raw.githubusercontent.com/MichaelTarasov21/Portfolio/master/src/Assets/VSCode.png",
-	Github: "https://raw.githubusercontent.com/MichaelTarasov21/Portfolio/master/src/Assets/Github-Light.svg",
-	Netlify: "https://raw.githubusercontent.com/MichaelTarasov21/Portfolio/master/src/Assets/Netlify.svg",
-	WebFrontendStack: "https://raw.githubusercontent.com/MichaelTarasov21/Portfolio/master/src/Assets/Web-Stack.png",
-	HTML: "https://raw.githubusercontent.com/MichaelTarasov21/Portfolio/master/src/Assets/HTML.svg",
-	CSS: "https://raw.githubusercontent.com/MichaelTarasov21/Portfolio/master/src/Assets/CSS.svg",
+	VSCode: "https://raw.githubusercontent.com/MichaelTarasov21/Portfolio/master/src/Assets/Development-Logos/VSCode.png",
+	Github: "https://raw.githubusercontent.com/MichaelTarasov21/Portfolio/master/src/Assets/Development-Logos/Github-Light.svg",
+	Netlify: "https://raw.githubusercontent.com/MichaelTarasov21/Portfolio/master/src/Assets/Development-Logos/Netlify.svg",
+	WebFrontendStack: "https://raw.githubusercontent.com/MichaelTarasov21/Portfolio/master/src/Assets/Development-Logos/Web-Stack.png",
+	HTML: "https://raw.githubusercontent.com/MichaelTarasov21/Portfolio/master/src/Assets/Development-Logos/HTML.svg",
+	CSS: "https://raw.githubusercontent.com/MichaelTarasov21/Portfolio/master/src/Assets/Development-Logos/CSS.svg",
 	Vue: "https://vuejs.org/images/logo.svg",
-	Firebase: "https://raw.githubusercontent.com/MichaelTarasov21/Portfolio/master/src/Assets/Firebase.svg",
+	Firebase: "https://raw.githubusercontent.com/MichaelTarasov21/Portfolio/master/src/Assets/Development-Logos/Firebase.svg",
 };
 
 export default logos;
