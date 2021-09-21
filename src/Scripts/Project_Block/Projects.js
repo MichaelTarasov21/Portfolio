384c4e280a7849059397846de59df6947fe55cdf
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2021-09-21 16:42:08 -0400

    Fixed issue with incorrectly named main js file for projects
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

---
+++ src/Scripts/Project_Block/Projects.js
@@ -0,0 +1,7 @@
+import Pokemon from "./Project-Html/Pokemon_Project.js";
+import CSS_Project from "./Project-Html/CSS_Project.js";
+import Escape_Room from "./Project-Html/Escape_Room_Project.js";
+
+const Projects = [Pokemon, CSS_Project, Escape_Room];
+
+export default Projects;
