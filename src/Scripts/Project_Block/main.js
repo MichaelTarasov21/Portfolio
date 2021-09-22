0271ec627273f14f35f47ae3468f5aae0ec88cd7
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2021-09-22 19:58:44 -0400

    Overhauled the way project sliding works
    The new system should eliminate all flashing as well as eliminate the need to reload images when changing projects.
    
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- src/Scripts/Project_Block/main.js
+++ src/Scripts/Project_Block/main.js
@@ -1,148 +1,153 @@
 import { DomSelectors } from "./DomSelectors.js";
 import Projects from "./Projects.js";
 import { ChangeIndex } from "./CyclicalPagination.js";
-function SelectImage(image, itemlist) {
-	for (let i = 0; i < itemlist.length; i++) {
-		// Removes the selected image tag from all images
-		const item = itemlist.item(i);
-		item.classList.remove("selectedimage");
-	}
-	image.classList.add("selectedimage");
 
-	document.getElementsByClassName("ActiveImage")[1].innerHTML = `<img class="activeimage" src="${image.src}" />`;
-}
-function MakeImagesClickable() {
-	const ActiveProjectImages = document.getElementsByClassName("sampleimage");
+let ActiveIndex = 0;
+let activeProject;
+let nextProject;
+let previousProject;
 
-	for (let i = 0; i < ActiveProjectImages.length; i++) {
-		const item = ActiveProjectImages.item(i);
-		item.addEventListener("click", function () {
-			SelectImage(item, ActiveProjectImages);
-		});
-	}
-}
-
-function OpenDetails(index) {
-	const location = document.getElementsByClassName("ProjectPopup").item(index);
-
-	DomSelectors.PopupBackground.style.display = "block";
-	document.body.classList.add("PopupOpen");
-	location.style.visibility = "visible";
-}
-function MakeDetailsOpenable() {
-	const OpenButtons = document.getElementsByClassName("OpenDetails");
-
-	for (let i = 0; i < OpenButtons.length; i++) {
-		const item = OpenButtons.item(i);
-		item.addEventListener("click", function () {
-			OpenDetails(i);
-		});
-	}
-}
-
-function CloseDetails(index) {
-	const location = document.getElementsByClassName("ProjectPopup").item(index);
-
-	DomSelectors.PopupBackground.style.display = "none";
-	document.body.classList.remove("PopupOpen");
-	location.style.visibility = "hidden";
-}
-function MakeDetailsClosable() {
-	const CloseButtons = document.getElementsByClassName("close");
-
-	for (let i = 0; i < CloseButtons.length; i++) {
-		const item = CloseButtons.item(i);
-		item.addEventListener("click", function () {
-			CloseDetails(i);
-		});
-	}
-}
-
-function SetUpProjects() {
-	Projects.forEach(function () {
+function insertProjects() {
+	function addProject(html, index) {
+		function AdjustScrollbar(index) {
+			// Hides unneeded scrollbars and enlarges the images to use the extra space
+			const imagebar = document.getElementsByClassName("ImageBar").item(index);
+			imagebar.scrollLeft = 1;
+			const moved = imagebar.scrollLeft;
+			if (moved === 0) {
+				imagebar.classList.add("NoScrollbar");
+			}
+		}
+		//Insert all the projects
+		document.getElementById("Projects").innerHTML = document.getElementById("Projects").innerHTML + html;
+		AdjustScrollbar();
 		//Create a new project indicator (little gray circle) for each project in Projects.js
 		document.getElementById("ProjectIndicator").innerHTML = document.getElementById("ProjectIndicator").innerHTML + `<span class="Indicator"></span>`;
-	});
-	document.getElementsByClassName("Indicator").item(0).classList.add("IndicatorSelected");
-
-	let ActiveIndex = 0;
-	let switching = false;
-	function ResetBlockPosition() {
-		const ProjectDiv = document.getElementById("Projects");
-		//Resets the block containg the projects to its default position after an animation so that the project stay in view
-		ProjectDiv.classList.remove("MoveLeft");
-		ProjectDiv.classList.remove("MoveRight");
-		ProjectDiv.style.overflow = "hidden";
-	}
-	function InsertProjects() {
-		// Switches project order after moving betwen projects in view
-		const PreviousProject = document.getElementById("PreviousProject");
-		const ActiveProject = document.getElementById("ActiveProject");
-		const NextProject = document.getElementById("NextProject");
-
-		// Placed here to prevent project blinking on slower computers
-		ActiveProject.innerHTML = Projects[ActiveIndex];
-		ResetBlockPosition();
-
-		PreviousProject.innerHTML = Projects[ChangeIndex(ActiveIndex, Projects.length, -1)];
-		NextProject.innerHTML = Projects[ChangeIndex(ActiveIndex, Projects.length, 1)];
-		MakeDetailsOpenable();
-		MakeDetailsClosable();
-		MakeImagesClickable();
-		AdjustScrollbar();
-		switching = false;
-	}
-	function ChangeIndicator() {
-		const ProjectIndicators = document.getElementsByClassName("Indicator");
-		for (let i = 0; i < ProjectIndicators.length; i++) {
-			ProjectIndicators.item(i).classList.remove("IndicatorSelected");
-		}
-		ProjectIndicators.item(ActiveIndex).classList.add("IndicatorSelected");
-	}
-	function SwitchProjects(direction) {
-		const ProjectDiv = document.getElementById("Projects");
-		ProjectDiv.style.overflow = "visible";
-		//Moves background left or right depending on arrow clicked
-		if (direction < 0) {
-			ProjectDiv.classList.add("MoveRight");
-		}
-		if (direction > 0) {
-			ProjectDiv.classList.add("MoveLeft");
-		}
-		setTimeout(InsertProjects, 700);
-	}
-	function AdjustScrollbar() {
-		// Hides unneeded scrollbars and resets the scrollbar position
-		let imagebar = document.getElementsByClassName("ImageBar").item(1);
-		imagebar.scrollLeft = 1;
-		const moved = imagebar.scrollLeft;
-		if (moved === 0) {
-			imagebar.classList.add("NoScrollbar");
-		}
-		imagebar.scrollLeft = 0;
-	}
-	function ChangeProject(amount) {
-		ActiveIndex = ChangeIndex(ActiveIndex, Projects.length, amount);
-		ChangeIndicator();
-		SwitchProjects(amount);
 	}
 
-	document.getElementById("MoveToPreviousProject").addEventListener("click", function () {
-		if (!switching) {
-			// Prevents double click
-			ChangeProject(-1);
-			switching = true;
-		}
-	});
-	document.getElementById("MoveToNextProject").addEventListener("click", function () {
-		if (!switching) {
-			// Prevents double click
-			ChangeProject(1);
-			switching = true;
-		}
-	});
+	Projects.forEach(addProject);
+	setProjects();
 
-	InsertProjects();
+	activeProject.style.display = "block";
+	// Highlight the first circle
+	document.getElementsByClassName("Indicator")[ActiveIndex].classList.add("IndicatorSelected");
+
+	function SelectImage(image, itemlist) {
+		for (let i = 0; i < itemlist.length; i++) {
+			// Removes the selected image tag from all images
+			const item = itemlist[i];
+			item.classList.remove("selectedimage");
+		}
+		image.classList.add("selectedimage");
+
+		document.getElementsByClassName("activeimage")[ActiveIndex].src = image.src;
+	}
+	function MakeImagesClickable() {
+		const ActiveProjectImages = document.getElementsByClassName("sampleimage");
+
+		for (let i = 0; i < ActiveProjectImages.length; i++) {
+			const item = ActiveProjectImages[i];
+			item.addEventListener("click", function () {
+				SelectImage(item, ActiveProjectImages);
+			});
+		}
+	}
+
+	function OpenDetails(index) {
+		const location = document.getElementsByClassName("ProjectPopup")[index];
+
+		DomSelectors.PopupBackground.style.display = "block";
+		document.body.classList.add("PopupOpen");
+		location.style.visibility = "visible";
+	}
+	function MakeDetailsOpenable() {
+		const OpenButtons = document.getElementsByClassName("OpenDetails");
+
+		for (let i = 0; i < OpenButtons.length; i++) {
+			const item = OpenButtons[i];
+			item.addEventListener("click", function () {
+				OpenDetails(i);
+			});
+		}
+	}
+
+	function CloseDetails(index) {
+		const location = document.getElementsByClassName("ProjectPopup")[index];
+
+		DomSelectors.PopupBackground.style.display = "none";
+		document.body.classList.remove("PopupOpen");
+		location.style.visibility = "hidden";
+	}
+	function MakeDetailsClosable() {
+		const CloseButtons = document.getElementsByClassName("close");
+
+		for (let i = 0; i < CloseButtons.length; i++) {
+			const item = CloseButtons[i];
+			item.addEventListener("click", function () {
+				CloseDetails(i);
+			});
+		}
+	}
+	MakeDetailsOpenable();
+	MakeDetailsClosable();
+	MakeImagesClickable();
+}
+function ChangeIndicator() {
+	const ProjectIndicators = document.getElementsByClassName("Indicator");
+	for (let i = 0; i < ProjectIndicators.length; i++) {
+		// Gray out all project indicators
+		ProjectIndicators[i].classList.remove("IndicatorSelected");
+	}
+	// Highlight the active indicator
+	ProjectIndicators[ActiveIndex].classList.add("IndicatorSelected");
+}
+function setProjects() {
+	const projectAmount = document.getElementsByClassName("Project").length;
+	activeProject = document.getElementsByClassName("Project")[ActiveIndex];
+	nextProject = document.getElementsByClassName("Project")[ChangeIndex(ActiveIndex, projectAmount, 1)];
+	previousProject = document.getElementsByClassName("Project")[ChangeIndex(ActiveIndex, projectAmount, -1)];
+}
+function moveToPrevious() {
+	const projectAmount = document.getElementsByClassName("Project").length;
+	ActiveIndex = ChangeIndex(ActiveIndex, projectAmount, -1);
+
+	activeProject.classList.add("MoveRightOut");
+	previousProject.classList.add("MoveRightIn");
+	ChangeIndicator();
+	setTimeout(endMove, 700);
+}
+function moveToNext() {
+	const projectAmount = document.getElementsByClassName("Project").length;
+	ActiveIndex = ChangeIndex(ActiveIndex, projectAmount, 1);
+
+	activeProject.classList.add("MoveLeftOut");
+	nextProject.classList.add("MoveLeftIn");
+	ChangeIndicator();
+	setTimeout(endMove, 700);
 }
 
-SetUpProjects();
+function endMove() {
+	if (activeProject.style.removeProperty) {
+		activeProject.style.removeProperty("display");
+	} else {
+		// For IE < 9
+		activeProject.style.removeAttribute("display");
+	}
+	activeProject.classList.remove("MoveRightOut");
+	activeProject.classList.remove("MoveLeftOut");
+
+	setProjects();
+
+	activeProject.style.display = "block";
+	activeProject.classList.remove("MoveLeftIn");
+	activeProject.classList.remove("MoveRightIn");
+	console.log(activeProject);
+	// Reset the scrollbar
+	const imagebar = document.getElementsByClassName("ImageBar").item(ActiveIndex);
+	imagebar.scrollLeft = 0;
+}
+
+insertProjects();
+
+document.getElementById("MoveToPreviousProject").addEventListener("click", moveToPrevious);
+document.getElementById("MoveToNextProject").addEventListener("click", moveToNext);
