dbcef2c7c746742241eb27d81d46f268734967be
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2021-11-15 19:54:25 -0500

    Changed project order
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- src/Scripts/Project_Block/Projects.js
+++ src/Scripts/Project_Block/Projects.js
@@ -2,6 +2,6 @@ import Pokemon from "./Project-Html/Pokemon_Project.js";
 import CSS_Project from "./Project-Html/CSS_Project.js";
 import Escape_Room from "./Project-Html/Escape_Room_Project.js";
 
-const Projects = [Pokemon.html(), CSS_Project.html(), Escape_Room.html()];
+const Projects = [Escape_Room.html(), CSS_Project.html(), Pokemon.html()];
 
 export default Projects;
